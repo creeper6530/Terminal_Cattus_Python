9962824deb237be7e985914b80498cc0ddbe3acd
creeper6530 <77244144+creeper6530@users.noreply.github.com>
2021-02-11 13:29:20 +0100

    Aktualizovány soubory

--- zadání_příkazu.py
+++ zadání_příkazu.py
@@ -1,4 +1,4 @@
-#Toto je deklarace příkazu "tisk".
+#Toto je deklarace funkce pro zadávání příkazů.
 #Je v odděleném souboru pro větší přehlednost kódu
 
 #_______________________________
@@ -7,6 +7,8 @@
 #Import příkazů
 from tisk import tisk
 from smazat import smazat
+from smazat_slozku import sm_slozku
+from obsah_slozky import obsh_slozky
 from pomoc import pomoc
 from akt_adr import adresář
 from zm_adr import zm_adresář
@@ -18,20 +20,41 @@ def zadání():
     comm = input(">>> ")
 
     #Detekce příkazu
-    if comm == "tisk":
+    if comm == "tisk": 
         obsah = input("Zadejte první argument: ")
         tisk(obsah) #Zavolá funkci "tisk" naimportovanou výše v programu bloku importující příkazy s argumentem v proměnné "obsah".
+    
+    
     elif comm == "smazat":
         soubor = input("Zadejte první argument: ")
         smazat(soubor) #Zavolá funkci "smazat" naimportovanou výše v bloku programu importující příkazy s argumentem v proměnné "soubor".
+    
+    
+    elif comm == "sm-slozku":
+        složka = input("Zadejte první argument: ")
+        sm_slozku(složka) #Zavolá funkci "sm_slozku" naimportovanou výše v bloku programu importující příkazy s argumentem v proměnné "složka".
+    
+    
+    elif comm == "obsah-sl":
+        obsh_slozky() #Zavolá funkci "obsh_slozky" naimportovanou výše v bloku programu importující příkazy.
+    
+    
     elif comm == "pomoc":
         pomoc() #Zavolá funkci "pomoc" naimportovanou výše v bloku programu importující příkazy.
+    
+    
     elif comm == "exit":
-        exit() #Okamžitě ukončí program. na tom není nic těžkého :-)
+        exit() #Okamžitě ukončí program. Na tom není nic těžkého :-)
+    
+    
     elif comm == "akt-adr":
         adresář() #Zavolá funkci "adresář" naimportovanou výše v bloku programu importující příkazy.
+    
+    
     elif comm == "zm-adr":
         adr = input("Zadejte první argument: ")
-        zm_adresář(adr) #Zavolá funkci "zm_adresář" naimportovanou výše v bloku programu importující příkazy.
+        zm_adresář(adr) #Zavolá funkci "zm_adresář" naimportovanou výše v bloku programu importující příkazy s argumentem v proměnné adr.
+    
+    
     else:
         print("Neznámý příkaz.")
