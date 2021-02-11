0ea813468b5b2e631a1f6f969b7d9868fa1f84c7
creeper6530 <77244144+creeper6530@users.noreply.github.com>
2021-02-11 13:30:55 +0100

    Aktualizovány soubory

--- pomoc.py
+++ pomoc.py
@@ -7,18 +7,20 @@
 #Deklarace funkce
 def pomoc():
     #Deklarace obsahu pomoci
-    pomoc = """Vítejte v nápovědě pro terminál Cattus!
+    pomoc = """Vítej v nápovědě pro terminál Cattus!
     
     Seznam příkazů:
     tisk                Vytiskne daný argument.
                         (Poznámka pro trouby: 'vytiskne' znamená vypíše do terminálu na další řádek.)
     smazat              Trvale smaže daný soubor. Soubor ke smazání deklaruješ argumentem.
                         Nezapomeň přidat příponu.
-                        Takže pokud máš soubor Wordu jménem "dopis", tak napiš do argumentu "dopis.docx" bez uvozovek.
-    pomoc               Vypíše tuto pomoc.
+    sm-slozku           Smaže složku. Z bespečnostních důvodů maže jen prázdné složky.
+    obsah-sl            Vypíše obsah složky, kde program právě pracuje.
+                        Příkaz ti dá na výběr, jestli chceš do výpisu zahrnout i složky.
     akt-adr             Vypíše umístění, kde program právě pracuje.
     zm-adr              Změní umístění, kde program právě pracuje.
                         Pokud chceš o úroveň výše, nastav první argument jako ".." bez uvozovek.
+    pomoc               Vypíše tuto pomoc.
     exit                Okamžitě ukončí program.
 
     Terminál Cattus lze vždy okamžitě ukončit klávesovou zkratkou Ctrl+C."""
