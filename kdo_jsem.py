1d7ce57765f3076ebe64527d82fe0a535003f199
creeper6530 <77244144+creeper6530@users.noreply.github.com>
2021-02-22 20:52:50 +0100

    Přidány programy

---
+++ kdo_jsem.py
@@ -0,0 +1,11 @@
+#Toto je deklarace příkazu "kdojsem".
+#Je v odděleném souboru pro větší přehlednost kódu
+
+#_______________________________
+
+#Deklarace funkce
+def kdojsemja(admin):
+    if admin == "True":
+        print("Jsem priveligiovaý uživatel.")
+    else:
+        print("Jsem normální uživatel.")
