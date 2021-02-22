7a2556e433a9d9600c8fc56c6ac7421e43b4c939
creeper6530 <77244144+creeper6530@users.noreply.github.com>
2021-02-22 20:23:15 +0100

    Přidán nový příkaz

---
+++ zm_uzv.py
@@ -0,0 +1,11 @@
+#Toto je deklarace příkazu "zm-uzv".
+#Je v odděleném souboru pro větší přehlednost kódu
+
+#_______________________________
+
+
+def zm_uzivatele(admin):
+    if admin == True:
+        admin = False
+    else:
+        admin = True
